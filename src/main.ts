c00c3c50ea2a8f4c6d532a5c6d2ef0076da752bd
eldarlrd <eldarlrd@pm.me>
2024-03-08 15:09:40 +0400

    chore: restructure

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { component } from 'riot';
 
 import 'normalize.css';
-import App from '@/app.riot';
+import Body from '@/body.riot';
 
-const mountApp = component(App);
-const app = document.getElementById('app');
-
-if (app) mountApp(app);
+const mountBody = component(Body);
+mountBody(document.body);
 
 // Easter Egg
 console.log(
